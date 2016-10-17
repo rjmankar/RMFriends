37e3212a968f0eb211cc869aaec2c08cc7dd45c6
rjmankar <rjmankar@yahoo.com>
2016-10-17 18:47:05 +0530

    initial commit

---
+++ RMFriends/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RMFriends
+//
+//  Created by Student P_05 on 17/10/16.
+//  Copyright © 2016 Rahul Mankar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
